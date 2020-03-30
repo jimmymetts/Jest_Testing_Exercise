b873f2830ad4b64574d8902e6934c2d516430f61
jimmymetts <jimmymetts@bellsouth.net>
2020-03-30 09:54:58 -0500

    changed test to NOT equal

--- functions.test.js
+++ functions.test.js
@@ -3,3 +3,7 @@ const functions = require('./functions');
 test('add 2 + 2 to equal 4', () => {
     expect(functions.add(2, 2)).toBe(4);
 });
+
+test('add 2 + 2 to NOT equal 4', () => {
+    expect(functions.add(2, 2)).toBe(4);
+});
