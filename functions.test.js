d5fe03873412d0c96872d2f4948a6f1d4582d9d5
jimmymetts <jimmymetts@bellsouth.net>
2020-03-30 10:02:21 -0500

    removed + 1 in add function

--- functions.test.js
+++ functions.test.js
@@ -4,6 +4,6 @@ test('add 2 + 2 to equal 4', () => {
     expect(functions.add(2, 2)).toBe(4);
 });
 
-test('add 2 + 2 to NOT equal 4', () => {
-    expect(functions.add(2, 2)).toBe(4);
+test('adds 2 + 2 to NOT equal 5', () => {
+    expect(functions.add(2, 2)).not.toBe(5);
 });
