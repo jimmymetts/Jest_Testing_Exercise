fc18456a3be6851220403f47c9e624bf17b51f76
jimmymetts <jimmymetts@bellsouth.net>
2020-03-30 14:21:46 -0500

    added comments/definitions

--- functions.test.js
+++ functions.test.js
@@ -7,3 +7,10 @@ test('add 2 + 2 to equal 4', () => {
 test('adds 2 + 2 to NOT equal 5', () => {
     expect(functions.add(2, 2)).not.toBe(5);
 });
+
+//CHECK FOR TRUTHY & FALSY VALUES
+// toBeNull matches only null
+// tonBeUndefined matches only undefined
+// toBeDefined is the opposite of toBeUndefined
+// toBeTruthy matches anything that an if statement treats as true
+// toBeFalsy matches anything that an if statement treats as false
