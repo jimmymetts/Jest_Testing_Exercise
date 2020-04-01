a98636fbcfac2dbf4ec70b18ae98433e18b7ad08
jimmymetts <jimmymetts@bellsouth.net>
2020-03-31 20:01:32 -0500

    added new test

--- functions.test.js
+++ functions.test.js
@@ -14,3 +14,7 @@ test('adds 2 + 2 to NOT equal 5', () => {
 // toBeDefined is the opposite of toBeUndefined
 // toBeTruthy matches anything that an if statement treats as true
 // toBeFalsy matches anything that an if statement treats as false
+
+test('adds 2 + 2 to NOT equal 5', () => {
+    expect(functions.add(2, 2)).not.toBe(5);
+});
