10d419539475df570670815fbca345cab8e04515
jimmymetts <jimmymetts@bellsouth.net>
2020-04-05 20:18:40 -0500

    added .not back

--- functions.test.js
+++ functions.test.js
@@ -43,5 +43,5 @@ test('Should be under 1600', () => {
 //Regular expressions
 
 test('There is no I in Team', () => {
-    expect('teami').toMatch(/I/i);
+    expect('teami').not.toMatch(/I/i);
 });
